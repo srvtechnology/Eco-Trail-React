609a7328fad6acba74bb6ff075fd282aec57ce22
jeetbasak <jeetbasak54@gmail.com>
2025-06-30 14:59:17 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 👈 Reset base
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
